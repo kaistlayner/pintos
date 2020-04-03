9dc0ecba6596a9d85b70a9e85bf6ff4aff465748
kaistlayner <layner@kaist.ac.kr>
2020-04-03 05:26:10 -0700

    before mlfqs

--- threads/build/threads/mmu.d
+++ threads/build/threads/mmu.d
@@ -3,5 +3,7 @@ threads/mmu.o: ../../threads/mmu.c ../../include/lib/stdbool.h \
  ../../include/threads/init.h ../../include/lib/debug.h \
  ../../include/lib/stdint.h ../../include/threads/pte.h \
  ../../include/threads/vaddr.h ../../include/threads/loader.h \
- ../../include/threads/palloc.h ../../include/threads/mmu.h \
- ../../include/intrinsic.h ../../include/threads/mmu.h
+ ../../include/threads/palloc.h ../../include/threads/thread.h \
+ ../../include/lib/kernel/list.h ../../include/threads/interrupt.h \
+ ../../include/threads/mmu.h ../../include/intrinsic.h \
+ ../../include/threads/mmu.h
