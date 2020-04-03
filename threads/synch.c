1bfa67d36f31cef5693abb7d743ffb3a53e231c1
hyojin1912 <hyojin@kaist.ac.kr>
2020-04-03 16:05:22 +0900

    debug

--- threads/synch.c
+++ threads/synch.c
@@ -130,7 +130,7 @@ sema_up (struct semaphore *sema) {
     	thread_unblock (list_entry (list_pop_front (&sema->waiters), struct thread, elem));
 	}
 	sema->value++;
-	yield_by_pri()
+	yield_by_pri();
 	intr_set_level (old_level);
 }
 
